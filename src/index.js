1695d33556a24e97b29101cf4983612be7e5a326
Firas Natour <frsnatour@hotmail.com>
2020-12-17 06:48:10 -0500

    Second commit

--- src/index.js
+++ src/index.js
@@ -9,8 +9,13 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
+    frame: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    }
   });
 
   // and load the index.html of the app.
