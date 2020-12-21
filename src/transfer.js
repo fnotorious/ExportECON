b07dea20064f6055d32963b2fd0008f8ca065b64
fnotorious <frsnatour@hotmail.com>
2020-12-20 22:59:11 -0500

    Almost there!

--- src/transfer.js
+++ src/transfer.js
@@ -1,18 +1,86 @@
-function transferInfo(country1, country2)
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const page_url = 'https://www.tutorialspoint.com/general_knowledge/general_knowledge_countries_with_capitals.htm';
+
+async function transferInfo(country)
 {
-  var python = require("python-shell");
-  var path = require("path")
+  const { data } = await axios.get(page_url);
+  const $ = cheerio.load(data);
 
-  var options = {
-    scriptPath : path.join(__dirname, 'scrape.py'),
-    args : [country1]
+  var $capital ;
+  var $currency ;
+  var countryName ;
+  var $name ;
+
+  if (country === "Bosnia and Herzegovina")
+  {
+    country = "Bosnia & Herzegovina" ;
   }
 
-  var options2 = {
-    scriptPath : path.join(__dirname, 'scrape.py'),
-    args : [country2]
+  if (country === "Papua New Guinea")
+  {
+    country = "Papa New Guinea";
   }
 
-  var country1Data[] = new python('scrape.py', options);
-  var country2Data[] = new python('scrape.py', options2);
+  if (country === "Côte d'Ivoire")
+  {
+    country = "Ivory Coast";
+  }
+
+  if (country === "Eswatini")
+  {
+    country = "Swaziland";
+  }
+
+  if (country === "Czechia")
+  {
+    country = "Czech Republic";
+  }
+
+  if (country === "North Macedonia")
+  {
+    country = "Macedonia";
+  }
+
+  for (var i = 1; i <= 194; i++)
+  {
+    countryName = $('body > div.mui-container-fluid.content > div > div.mui-col-md-6.tutorial-content > table > tbody > tr:nth-child(' + i + ') > td:nth-child(1)');
+    $name = $(countryName);
+
+    if (($name.text().trim()).includes(country))
+    {
+      $capital = $($('body > div.mui-container-fluid.content > div > div.mui-col-md-6.tutorial-content > table > tbody > tr:nth-child(' + i + ') > td:nth-child(2)')).text().trim();
+      $currency = $($('body > div.mui-container-fluid.content > div > div.mui-col-md-6.tutorial-content > table > tbody > tr:nth-child(' + i + ') > td:nth-child(3)')).text().trim();
+    }
+  }
+
+  if (country === "Romania")
+  {
+    $currency = "Leu";
+  }
+
+  else if (country === "Croatia")
+  {
+    $currency = "Kuna";
+  }
+
+  else if (country === "Republic of the Congo")
+  {
+    $currency = "Congolese Franc";
+  }
+
+  else if (country === "Bolivia")
+  {
+    $currency = "Bolivian Boliviano"
+    $capital = "La Paz; Sucre"
+  }
+
+  else if (country === "Guinea")
+  {
+    $currency = "Guinea Franc";
+    $capital = "Conakry";
+  }
+
+  return await [$capital, $currency];
 }
