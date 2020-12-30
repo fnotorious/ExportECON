928699701e2e00ada56f04c9d993c8d25f3c2bed
fnotorious <frsnatour@hotmail.com>
2020-12-29 23:42:14 -0500

    Making edits for documentation

--- src/transfer.js
+++ src/transfer.js
@@ -1,17 +1,37 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+/**
+* This file is used for transferring important data, including a country's
+* capital, currency, GDP, GDP per capita, etc., to the browser side of the app.
+*/
+
+const axios = require('axios');                 // get Axios API
+const cheerio = require('cheerio');             // get Cheerio API
+
+// storing url of website to scrape for getting capital and currency data
 
 const page_url = 'https://www.tutorialspoint.com/general_knowledge/general_knowledge_countries_with_capitals.htm';
 
+/**
+* Asynchronous function for retrieving the capital and currency data of a
+* nation.
+*
+* @param {String} country the name of a country
+* @return {Array} an array consisting of the capital and currency of a country
+*/
 async function transferInfo(country)
 {
-  const { data } = await axios.get(page_url);
-  const $ = cheerio.load(data);
+  const { data } = await axios.get(page_url);   // request for stored url
+  const $ = cheerio.load(data);                 // for selecting elements within HTML data from url
 
-  var $capital ;
-  var $currency ;
-  var countryName ;
-  var $name ;
+  var $capital ;            // to store the capital city of a country
+  var $currency ;           // to store the currency of a country
+  var countryName ;         // to store the selector of a country name from the url's HTML data
+  var $name ;               // to store the actual name of a country as a String taken from the countryName variable
+
+  // Sometimes, the user may select a country whose name does not exactly
+  // match the name of a country within the url's data (i.e. '&' used instead of
+  // 'and', 'Czech Republic' instead of 'Czechia', etc.) These if statements
+  // are provided to prevent any errors that may occur because of such minor
+  // differences in how the countries are named.
 
   if (country === "Bosnia and Herzegovina")
   {
@@ -38,17 +58,32 @@ async function transferInfo(country)
     country = "Macedonia";
   }
 
+  // A loop that will iterate throughout a table within the stored url's HTML
+  // that contains the data we are looking for. (country name, capital, and
+  // currency)
+
   for (var i = 1; i <= 194; i++)
   {
+    // get selector for the i'th index of the table
+
     countryName = $('body > div.mui-container-fluid.content > div > div.mui-col-md-6.tutorial-content > table > tbody > tr:nth-child(' + i + ') > td:nth-child(1)');
     $name = $(countryName);
 
+    // if the country name at the current index in the table matches
+    // the country we're looking for
+
     if (($name.text().trim()).includes(country))
     {
+      // retrieve the capital and currency data from this table's index
+
       $capital = $($('body > div.mui-container-fluid.content > div > div.mui-col-md-6.tutorial-content > table > tbody > tr:nth-child(' + i + ') > td:nth-child(2)')).text().trim();
       $currency = $($('body > div.mui-container-fluid.content > div > div.mui-col-md-6.tutorial-content > table > tbody > tr:nth-child(' + i + ') > td:nth-child(3)')).text().trim();
     }
   }
+
+  // Within the url, some of the currencies and capitals of some countries
+  // happen to be incorrect. These if statements are meant to prevent error
+  // for such cases.
 
   if (country === "Romania")
   {
@@ -77,9 +112,17 @@ async function transferInfo(country)
     $capital = "Conakry";
   }
 
+  // return the capital and currency of a country.
+
   return await [$capital, $currency];
 }
 
+/**
+* Asynchronous function for retrieving the GDP of a country
+*
+* @param {String} country the name of a country
+* @return {String} the GDP of a country
+*/
 async function getGDP(country)
 {
   const { data } = await axios.get("https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)");
@@ -289,6 +332,11 @@ async function ExportPartner(country)
     country = "Bosnia";
   }
 
+  if (country === "Eswatini")
+  {
+    country = "Swaziland";
+  }
+
   for (var i = 1; i <= 194; i++)
   {
     countryName = $('#mw-content-text > div.mw-parser-output > table:nth-child(9) > tbody > tr:nth-child(' + i + ') > td:nth-child(1) > a');
@@ -335,6 +383,11 @@ async function ImportPartner(country)
   if (country === "Bosnia and Herzegovina")
   {
     country = "Bosnia";
+  }
+
+  if (country === "Eswatini")
+  {
+    country = "Swaziland";
   }
 
   for (var i = 1; i <= 194; i++)
